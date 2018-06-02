b6acddfbef70a56a8fc5bb34bdcf3a7a170d641c
dan <danrleyt@gmail.com>
2018-06-02 15:42:29 -0300

    New functions and comments

--- recommend.py
+++ recommend.py
@@ -9,6 +9,7 @@ result = os.listdir('../result/')
 test_arr = os.listdir('../test_def/')
 train_arr = os.listdir('../training/')
 
+#acha o pid da melhor candidata
 def findBestCands(pid):
     #acessa o arquivo
     with open('../result/'+str(pid)+".json") as p:
@@ -19,22 +20,23 @@ def findBestCands(pid):
         if (c["jac"] > 0.2):
             res.append(c)
     return res
-            
+
+#acessa a melhor candidata e retorna todas as tracks dela     
 def findPl(cands):
     for f in train_arr:
         s = f.replace('mpd.slice.','')
         s = s.replace('.json', '')
         a,b = s.split('-')
+        tracks = []
         if int(cands) > int(a) and int(cands) < int(b):
             with open('../training/'+f) as pl:
                 pl_data = json.load(pl)
             for p in pl_data["playlists"]:
                 if p["pid"] == cands:
-                    print p
+                    tracks = p["tracks"]
+                    return tracks
         else:
             next
-            
-
 
 iterator = 0
 for f in test_arr:  
@@ -43,11 +45,16 @@ for f in test_arr:
     cands = []
     for p in pl_data["playlists"]:
         iterator += 1
+        #agora eu tenho o pid da playlist e preciso procurar as candidatas
         aux = findBestCands(p["pid"])
         if aux:
             for a in aux: 
                 if a["jac"] > 0.2:
+                    print a
                     findPl(a["cand"])
+                    #crio uma funcao aqui que recebe as tracks e uma playlist
+                    #retorna a playlist completa e escreve num arquivo
+
                 
     
 
