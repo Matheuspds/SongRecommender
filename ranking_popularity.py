a55933fef5bbb88cca77c0a77cb308a637416238
dan <danrleyt@gmail.com>
2018-06-06 20:36:15 -0300

    generate ranking

--- ranking_popularity.py
+++ ranking_popularity.py
@@ -11,15 +11,19 @@ iterator_tr = 0
 frames_tr = []
 for js in training_arr:
     print "file "+str(iterator_tr)
-        
     with open('../training/'+js) as f:
         d = json.load(f)
     iterator_tr += 1
     df = json_normalize(d["playlists"], record_path="tracks", meta=["pid", "num_tracks"])
     frames_tr.append(df)
     train_data = pd.concat(frames_tr)
-     
-train = train_data[["track_uri","pid"]]
-train = train.groupby("pid")["track_uri"].apply(list)
 
-print train
+train = train_data[["track_uri","track_name","pid"]]
+rank = train["track_uri"].value_counts()
+#train = train.groupby("pid")["track_uri"].apply(list)
+
+tr = train.groupby(["track_uri", "track_name"]).size().reset_index()
+tr.columns = ["track_uri", "track_name", "frequency"]
+out = tr.to_json(orient='records')
+with open("../ranking/ranking.json", "w") as f:
+    f.write(out)
